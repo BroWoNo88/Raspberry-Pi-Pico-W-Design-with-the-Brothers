f0e15f0df33cc0feaa4de3072cf19b323fc7dc2c
OSYCh314 <111153351+OSYCh314@users.noreply.github.com>
2026-08-25 20:59:09 +1000

    Update Sensor_code.py

--- Sensor_code.py
+++ Sensor_code.py
@@ -1,5 +1,6 @@
 from machine import Pin, PWM
 from I2C_LCD import screen
+from RGB_LED import rgb_led
 
 def get_colour(bounds,value,inverted = False):
   if value < bounds[0] or value > bounds[-1]:
@@ -24,3 +25,6 @@ lcd = screen()
 lcd.start()
 
 #Set up the RGB LEDs
+temp_led = rgb_led(3,4,5)
+
+hum_led = rgb_led(6,7,8)
