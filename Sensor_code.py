e5a7f95c8dcc1504ef2cf18f6df99cd88a13f7cf
OSYCh314 <111153351+OSYCh314@users.noreply.github.com>
2026-08-25 21:00:53 +1000

    Refactor get_colour function to return RGB tuples

--- Sensor_code.py
+++ Sensor_code.py
@@ -4,21 +4,21 @@ from RGB_LED import rgb_led
 
 def get_colour(bounds,value,inverted = False):
   if value < bounds[0] or value > bounds[-1]:
-      return 'INV'
+      return (128,0,128)
   if inverted:
     if value < bounds[1] or value > bounds[-2]:
-      return 'GRN'
+      return (0,255,0)
     elif value < bounds[2] or value > bounds[-3]:
-      return 'YLW'
+      return (255,255,0)
     else:
-      return 'RED'
+      return (255,0,0)
   else:
     if value < bounds[1] or value > bounds[-2]:
-      return 'RED'
+      return (255,0,0)
     elif value < bounds[2] or value > bounds[-3]:
-      return 'YLW'
+      return (255,255,0)
     else:
-      return 'GRN'
+      return (0,255,0)
 
 #Set up the display
 lcd = screen()
