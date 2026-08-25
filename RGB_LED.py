6dae4a733e1bd19244a335d83b5f14fb09a85501
OSYCh314 <111153351+OSYCh314@users.noreply.github.com>
2026-08-25 20:58:08 +1000

    Create RGB_LED.py
    Added RGB_LED module

---
+++ RGB_LED.py
@@ -0,0 +1,22 @@
+from machine import Pin, PWM
+
+class rgb_led:
+  def __init__(self,pin_r,pin_g,pin_b,freq = 1000):
+    self.r = PWM(Pin(pin_r))
+    self.g = PWM(Pin(pin_g))
+    self.b = PWM(Pin(pin_b))
+
+    self.r.freq(freq)
+    self.g.freq(freq)
+    self.b.freq(freq)
+
+  def show_colour(self,r,g,b):
+    #Converts from 0-255 to 65535-0
+    r_true = (255 - r)*256
+    self.r.duty_u16(r_true)
+
+    g_true = (255 - g)*256
+    self.g.duty_u16(g_true)
+
+    b_true = (255 - b)*256
+    self.b.duty_u16(b_true)
