4f1fb162b53011e04c21ce1ed63d5006e929eeb5
OSYCh314 <111153351+OSYCh314@users.noreply.github.com>
2026-08-25 20:53:25 +1000

    Update Sensor_code.py
    Added a colour function

--- Sensor_code.py
+++ Sensor_code.py
@@ -1,6 +1,26 @@
 from machine import Pin, PWM
 from I2C_LCD import screen
 
+def get_colour(bounds,value,inverted = False):
+  if value < bounds[0] or value > bounds[-1]:
+      return 'INV'
+  if inverted:
+    if value < bounds[1] or value > bounds[-2]:
+      return 'GRN'
+    elif value < bounds[2] or value > bounds[-3]:
+      return 'YLW'
+    else:
+      return 'RED'
+  else:
+    if value < bounds[1] or value > bounds[-2]:
+      return 'RED'
+    elif value < bounds[2] or value > bounds[-3]:
+      return 'YLW'
+    else:
+      return 'GRN'
+
 #Set up the display
 lcd = screen()
-lcd.init()
+lcd.start()
+
+#Set up the RGB LEDs
