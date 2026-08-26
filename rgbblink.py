e20f6ecb8519eac85e4b74536875edeb774cd2ae
OSYCh314 <111153351+OSYCh314@users.noreply.github.com>
2026-08-27 08:53:22 +1000

    Update rgbblink.py
    For James: We forgot to put the module files on the Pico Pi, that's why it didn't work. If we are to do this, remember to put the module files on the Pico (I2C_LCD.py and RGB_LED.py)

--- rgbblink.py
+++ rgbblink.py
@@ -1,38 +1,7 @@
-from machine import Pin, PWM
 from time import sleep
 import random
+from RGB_LED import rgb_led
 
-class rgb_led:
-  def __init__(self,pin_r,pin_g,pin_b,freq = 1000):
-    self.r = PWM(Pin(pin_r))
-    self.g = PWM(Pin(pin_g))
-    self.b = PWM(Pin(pin_b))
-
-    self.r.freq(freq)
-    self.g.freq(freq)
-    self.b.freq(freq)
-
-  def show_colour(self,rgb_tuple):
-    #Converts from 0-255 to 65535-0
-    r,g,b = rgb_tuple
-    
-    r_true = r*256
-    self.r.duty_u16(r_true)
-
-    g_true = g*256
-    self.g.duty_u16(g_true)
-
-    b_true = b*256
-    self.b.duty_u16(b_true)
-
-def onOrOff():
-    num = random.randint(1, 2)
-    if num == 1:
-        return 0
-    else:
-        return 255
-
-#Initialisation
 led = rgb_led(0,1,2)
 #Main loop
 for i in range(1000):
